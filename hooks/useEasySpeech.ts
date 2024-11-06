2fc827fcc6222bbf94514e7be2ae6c13af7168c4
Ilinykh Ivan <ilinykh.i@skbkontur.ru>
2024-11-06 13:56:28 +0400

    make Easy Speech init faster

--- hooks/useEasySpeech.ts
+++ hooks/useEasySpeech.ts
@@ -3,21 +3,19 @@
 import { useEffect, useState } from 'react';
 import EasySpeech from 'easy-speech';
 
+typeof window !== 'undefined' && EasySpeech.init({ maxTimeout: 5000, interval: 250 });
+
 export function useEasySpeech() {
   const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
-    // Ensure this runs only on the client
-    if (typeof window !== 'undefined') {
-      EasySpeech.init({ maxTimeout: 5000, interval: 250 })
-        .then(() => {
-          setIsInitialized(true);
-        })
-        .catch((e) => {
-          console.error('Error initializing EasySpeech:', e);
-        });
-    }
+    typeof window !== 'undefined' &&
+      EasySpeech.init({ maxTimeout: 5000, interval: 250 }).then(() => {
+        setIsInitialized(true);
+      });
   }, []);
 
-  return { easySpeech: typeof window !== 'undefined' && isInitialized ? EasySpeech : null };
+  return {
+    easySpeech: typeof window !== 'undefined' && isInitialized ? EasySpeech : null,
+  };
 }
